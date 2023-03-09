e8f45cbd70809f617a654c85263c99bf246af672
Leandro Cavalcante <devleandromcz@gmail.com>
2023-03-09 17:28:43 -0300

    questions

---
+++ thehuxley-17.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    
+    int number;
+    scanf("%d", &number);
+    int number1 = number - 1;
+    int number2 = number + 1;
+    printf("%d %d\n", number1, number2);
+    
+	return 0;
+}
